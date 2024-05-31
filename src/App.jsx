ded66e777d8a84563377b338849a43a260cb7e16
Ahmed-Al-Badri <albadri@pdx.edu>
2024-05-31 11:31:52 -0700

    "Information is set up, except graphs"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { configDotenv } from "dotenv";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-
+import "./bodystyle/styles.css";
 import Nav_ from "./Nav/Nav";
 import Route_ from "./Route/Route";
 
@@ -10,10 +10,10 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="row">
+        <div className="row NAV">
           <Nav_ />
         </div>
-        <div className="row">
+        <div className="row ROUTE">
           <Route_ />
         </div>
       </div>
